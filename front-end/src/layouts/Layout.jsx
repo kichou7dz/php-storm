570f77ba9cb9ea314d7c944278372304fdbbfafe
kichou7dz <sofiane2020kichou@gmail.com>
2025-10-17 19:54:09 +0100

    test

---
+++ front-end/src/layouts/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <>
+            <header>Header</header>
+            <main>
+
+<Outlet/>
+
+            </main>
+            <footer>Footer</footer>
+        </>
+    );
+}
